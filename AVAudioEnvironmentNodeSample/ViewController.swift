1a55ebabfed752c28cd2af0636abe3a6f831a948
Katsuya Endoh <studio@enkatsu.org>
2022-12-02 02:39:11 +0900

    Initial Commit

---
+++ AVAudioEnvironmentNodeSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AVAudioEnvironmentNodeSample
+//
+//  Created by Katsuya Endoh on 2022/12/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
